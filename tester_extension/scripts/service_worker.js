0686635a171f9b460bdcdd1743c37ad7fe7e96da
warrenjx <warrenjxiong@gmail.com>
2024-02-08 14:45:58 -0500

    added code to trigger tab switch to next one, currently just runs on icon click

--- tester_extension/scripts/service_worker.js
+++ tester_extension/scripts/service_worker.js
@@ -4,22 +4,58 @@ function do_something() {
   // turn background color green
   document.body.style.backgroundColor = 'green';
 }; 
-
-// 
+ 
 chrome.action.onClicked.addListener((tab) => {
-  if (!tab.url.includes('chrome://')) {
+  /*if (!tab.url.includes('chrome://')) {
     // execute random script
-    /*chrome.scripting.executeScript({
+    chrome.scripting.executeScript({
       target: { tabId: tab.id }, 
       func: do_something
-    });*/
+    });
+  } */
     // not moving tabs but the arrow keys
     // go forward
     //chrome.tabs.goForward(tab.tabId); 
     // go backward
     //chrome.tabs.goBack(tab.tabId); 
-    
-    // change tabs
-    chrome.tabs.move(tab.tabId, { index: -1 }); 
-  } 
+
+  // change tabs forward
+  /*chrome.windows.getLastFocused(
+    {populate: true}, 
+    function(window) {
+      var n = window.tabs.length; 
+
+      for (var i = 0; i < n; i++) {
+        if (window.tabs[i].active) {
+          if (i == n - 1) {
+            chrome.tabs.update(window.tabs[0].id, { active: true }); 
+          } else {
+            chrome.tabs.update(window.tabs[i + 1].id, { active: true }); 
+          }
+
+          return; 
+        } 
+      }
+    }
+  );*/
+
+  // change tabs backward
+  chrome.windows.getLastFocused(
+    {populate: true}, 
+    function(window) {
+      var n = window.tabs.length; 
+
+      for (var i = 0; i < n; i++) {
+        if (window.tabs[i].active) {
+          if (i == 0) {
+            chrome.tabs.update(window.tabs[n - 1].id, { active: true }); 
+          } else {
+            chrome.tabs.update(window.tabs[i - 1].id, { active: true }); 
+          }
+
+          return; 
+        }
+      }
+    }
+  );
 }); 
