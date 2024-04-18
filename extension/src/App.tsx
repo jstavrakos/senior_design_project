12e4b97645229d00decd940db6019b8fa8692752
Jack Stavrakos <jstavrak@purdue.edu>
2024-04-18 17:26:20 -0400

    unnest my_chrome_extension folder and add everything back

---
+++ extension/src/App.tsx
@@ -0,0 +1,176 @@
+import { useState, useEffect, RefObject, createRef } from 'react';
+import Webcam from "react-webcam";
+
+// Interface for video constraints with type safety
+interface VideoConstraints {
+  width: number;
+  height: number;
+  facingMode: FacingMode;
+}
+
+// Enum for facingMode property, ensuring correct values
+enum FacingMode {
+  USER = 'user',
+  ENVIRONMENT = 'environment',
+}
+
+const videoConstraints: VideoConstraints = {
+  width: 150,
+  height: 150,
+  facingMode: FacingMode.USER,
+}
+
+export default function App() {
+  const [webCamState, setWebCamState] = useState(false);
+  const [frameCaptureState, setFrameCaptureState] = useState(false);
+  const [imageSrc, setImageSrc] = useState<string | null>(null); // Image source not displayed anymore as data is sent to off_screen.tsx
+  const [outputArray, setOutputArray] = useState< number[]| null>(null);
+  const webcamRef = createRef<Webcam>();
+
+  // Update the webcam state and frame capture state from the background script
+  useEffect(() => {
+    setupOffscreenDocument('off_screen.html');
+    chrome.runtime.sendMessage({ message: 'useEffect'}, function(response) {
+      console.log('Response from off_screen.js:', response);
+      if (response && response.webCamState !== undefined) {
+        setWebCamState(response.webCamState);
+      }
+      if (response && response.frameCaptureState !== undefined) {
+        setFrameCaptureState(response.frameCaptureState);
+      }
+    });
+  }, []);
+
+  // Listen for messages from the background script
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.message !== undefined) {
+      if (message.message === 'frameCaptureState') {
+        setOutputArray(message.results);
+      }
+    }
+  });
+
+  return (
+    <div className="mx-auto max-w-lg p-6 bg-gray-100 rounded-lg shadow-md">
+    <h1 className="text-3xl font-semibold text-center mb-6">Welcome to the Gesture App</h1>
+    <button 
+      className="block w-full py-2.5 px-5 mb-4 text-center text-gray-900 bg-gray-200 border border-gray-800 rounded-lg hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium"
+      onClick={() => {
+        if(webCamState){
+          setWebCamState(false);
+          setFrameCaptureState(false);
+          chrome.runtime.sendMessage({ message: 'frameCaptureState', frameCaptureState: false });
+        }
+        else {
+          setWebCamState(true);
+        }
+        chrome.runtime.sendMessage({ message: 'webCamState', webCamState: !webCamState });
+      }}>
+      {webCamState ? 'Stop' : 'Start'} Webcam
+    </button>
+    <br />
+    <button 
+      className="block w-full py-2.5 px-5 mb-4 text-center text-white bg-blue-500 border border-blue-700 rounded-lg font-bold hover:bg-blue-700 focus:outline-none"
+      onClick={() => {
+        if (webCamState && !frameCaptureState) {
+          setFrameCaptureState(true); 
+          chrome.runtime.sendMessage({ message: 'frameCaptureState', frameCaptureState: true });
+        } else {
+          setFrameCaptureState(false);
+          chrome.runtime.sendMessage({ message: 'frameCaptureState', frameCaptureState: false });
+        }
+      }}>
+      {(frameCaptureState) ? 'Stop' : 'Start'} Frame Capture
+    </button>
+    <div className="flex items-center justify-center">
+      {webCamState && <div className="mx-auto"><RenderWebcam webcamRef={webcamRef} /></div>}
+      {frameCaptureState && imageSrc && <img className="mx-auto" src={imageSrc} alt="Captured" />}
+    </div>
+    <div className="flex justify-between">
+      <button 
+        className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+        onClick={changeTabLeft}>
+        Left
+      </button>
+      <button 
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={changeTabRight}>
+        Right
+      </button>
+    </div>
+    {outputArray && (
+      <div className="mt-4">
+        <p>Highest Confidence Object Detected Class: {outputArray[0]}</p>
+      </div>
+    )}
+  </div>  
+  );
+};
+
+
+function changeTabLeft() {
+  chrome.tabs.query({ currentWindow: true }, (tabs) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (currentTab) => {
+      const currentIndex = tabs.findIndex((tab) => tab.id === currentTab[0].id);
+      const newIndex = (currentIndex - 1 + tabs.length) % tabs.length;
+      if (tabs[newIndex].id !== undefined) {
+        chrome.tabs.update(tabs[newIndex].id!, { active: true });
+      }
+    });
+  });
+}
+
+function changeTabRight() {
+  chrome.tabs.query({ currentWindow: true }, (tabs) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (currentTab) => {
+      const currentIndex = tabs.findIndex((tab) => tab.id === currentTab[0].id);
+      const newIndex = (currentIndex + 1) % tabs.length;
+      if (tabs[newIndex].id !== undefined) {
+        chrome.tabs.update(tabs[newIndex].id!, { active: true });
+      }
+    });
+  });
+}
+
+function RenderWebcam(props: { webcamRef: RefObject<Webcam> }) {
+  return (
+    <div>
+      <Webcam
+        audio={false}
+        minScreenshotHeight={150}
+        minScreenshotWidth={150}
+        screenshotFormat="image/jpeg"
+        videoConstraints={videoConstraints}
+        ref={props.webcamRef}
+      />
+    </div>
+  );
+}
+
+let creating: (Promise<void>) | null; // A global promise to avoid concurrency issues
+async function setupOffscreenDocument(path: string) {
+  // Check all windows controlled by the service worker to see if one
+  // of them is the offscreen document with the given path
+  const offscreenUrl = chrome.runtime.getURL(path);
+  const existingContexts = await chrome.runtime.getContexts({
+    contextTypes: [chrome.runtime.ContextType.OFFSCREEN_DOCUMENT],
+    documentUrls: [offscreenUrl]
+  });
+
+  if (existingContexts.length > 0) {
+    return;
+  }
+
+  // create offscreen document
+  if (creating) {
+    await creating;
+  } else {
+    creating = chrome.offscreen.createDocument({
+      url: path,
+      reasons: [chrome.offscreen.Reason.USER_MEDIA],
+      justification: 'get user media for gesture recognition',
+    });
+    await creating;
+    creating = null;
+  }
+}
---
+++ extension/webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    entry: {
+        index: './src/index.tsx',
+        options: './src/options.tsx',
+        background: './src/background.ts',
+        content: './src/content.tsx',
+        off_screen: './src/off_screen.tsx',
+    },
+    target: ['web'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        chunkFilename: '[id].bundle_[chunkhash].js',
+        libraryTarget: 'umd',
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(tsx|ts)$/,
+                exclude: /node_modules/,
+                use: 'ts-loader'
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/i,
+                include: path.resolve(__dirname, 'src'),
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.svg$/i,
+                use: [
+                    {
+                        loader: 'svg-url-loader',
+                        options: {
+                            limit: 10000,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'popup.html',
+            chunks: ['index'],
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/options.html',
+            filename: 'options.html',
+            chunks: ['options'],
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/off_screen.html',
+            filename: 'off_screen.html',
+            chunks: ['off_screen'],
+        }),
+        new CopyPlugin({
+            patterns: [
+                // {from: './public/options.html', to: 'options.html'},
+                // {from: './src/options.js', to: 'options.js'},
+                {from: './public/hand.png', to: 'hand.png'},
+                {from: './public/manifest.json', to: 'manifest.json'},
+                {from: './src/custom.onnx', to: 'custom.onnx'},
+                {from: './node_modules/onnxruntime-web/dist/*.wasm', to: '[name][ext]'}
+            ],
+        }),
+        // new BundleAnalyzerPlugin()
+    ],
+};
